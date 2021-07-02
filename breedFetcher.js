6e95064fab6a9ac6002ce3097cc04d9c43acaa7c
Raphaella Robles <raphaella.robles@gmail.com>
2021-07-02 04:10:40 +0000

    Refactored breedFetcher.js to be called from different files

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,29 +1,27 @@
 const request = require('request');
-const breed = process.argv[2]; // command line argument input
-const url = `https://api.thecatapi.com/v1/breeds/search?q=${breed}`;
 
-const breedFetcher = (url, breed) => {
+const fetchBreedDescription = (breed, callback) => {
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breed}`;
   request(url, (error, response, body) => {
     // Print the response status code if a response was received
     console.log('statusCode:', response && response.statusCode);
-    //need to parse string, which returns it as an object
-    const data = JSON.parse(body);
-    // TODO: Edge Case: Request Failed
-    if (error) {
-      console.log('error:', error); // Print the error if one occurred
+    if (error){
+      callback(error, null);
       return;
     }
+    //need to parse string, which returns it as an object
+    const data = JSON.parse(body);
     // TODO: Edge Case: Breed Not Found
     if (data.length === 0){ // Finds there is no object within data
-      console.log("no breed found");
+      const err1 = new Error('No breed found');
+      callback(err1, null);
       return;
     }
     // from data array, find the value of the description key
     const description = data[0]["description"];
     // print out the description of the breed to the terminal
-    console.log(description);
-    console.log(typeof description);
+    callback(null, description);
   })
 };
 
-breedFetcher(url, breed);
+module.exports = { fetchBreedDescription };
